9f4a5c5b163acedead0abefa44486681cbc4635d
pinales946@gmail.com <pinales946@gmail.com>
2023-06-24 15:10:24 -0400

    primer commit

---
+++ LearningChat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearningChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
